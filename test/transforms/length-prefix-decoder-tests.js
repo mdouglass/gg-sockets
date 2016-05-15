02052641d44677d710f328b729baf223f458587a
Matthew Douglass <matthew@douglass.email>
2016-05-14 21:26:26 -0700

    Switch to 4-byte little endian prefix

--- test/transforms/length-prefix-decoder-tests.js
+++ test/transforms/length-prefix-decoder-tests.js
@@ -8,33 +8,35 @@ describe('LengthPrefixDecoder', () => {
 
   it('has no outputs if the input is an incomplete length', () => {
     const t = new LengthPrefixDecoder()
-    t.write(Buffer.from([ 0x00 ]))
+    t.write(Buffer.from([ 0x00, 0x00, 0x01 ]))
     expect(t.read()).equals(null)
   })
 
   it('has no outputs if the input is complete length and an incomplete message', () => {
     const t = new LengthPrefixDecoder()
-    t.write(Buffer.from([ 0x00, 0x01 ]))
+    t.write(Buffer.from([ 0x00, 0x00, 0x00, 0x01 ]))
     expect(t.read()).equals(null)
   })
 
   it('outputs the message if the input is a complete message', () => {
     const t = new LengthPrefixDecoder()
-    t.write(Buffer.from([ 0x00, 0x01, 0x65 ]))
+    t.write(Buffer.from([ 0x01, 0x00, 0x00, 0x00, 0x65 ]))
     expect(t.read()).deep.equals(Buffer.from([ 0x65 ]))
   })
 
   it('outputs multiple message if the input is multiple messages', () => {
     const t = new LengthPrefixDecoder()
-    t.write(Buffer.from([ 0x00, 0x01, 0x65, 0x00, 0x02, 0x66, 0x67 ]))
+    t.write(Buffer.from([ 0x01, 0x00, 0x00, 0x00, 0x65, 0x02, 0x00, 0x00, 0x00, 0x66, 0x67 ]))
     expect(t.read()).deep.equals(Buffer.from([ 0x65 ]))
     expect(t.read()).deep.equals(Buffer.from([ 0x66, 0x67 ]))
   })
 
   it('outputs message that is reassembled from multiple inputs', () => {
     const t = new LengthPrefixDecoder()
-    t.write(Buffer.from([ 0x00 ]))
     t.write(Buffer.from([ 0x01 ]))
+    t.write(Buffer.from([ 0x00 ]))
+    t.write(Buffer.from([ 0x00 ]))
+    t.write(Buffer.from([ 0x00 ]))
     t.write(Buffer.from([ 0x65 ]))
     expect(t.read()).deep.equals(Buffer.from([ 0x65 ]))
   })
--- test/transforms/length-prefix-encoder-tests.js
+++ test/transforms/length-prefix-encoder-tests.js
@@ -9,14 +9,14 @@ describe('LengthPrefixEncoder', () => {
   it('outputs length-prefixed messages for the input', () => {
     const t = new LengthPrefixEncoder()
     t.write('ABC')
-    expect(t.read()).deep.equals(Buffer.from([ 0x00, 0x03, 0x41, 0x42, 0x43 ]))
+    expect(t.read()).deep.equals(Buffer.from([ 0x03, 0x00, 0x00, 0x00, 0x41, 0x42, 0x43 ]))
   })
 
   it('outputs length-prefixed messages for multiple outputs', () => {
     const t = new LengthPrefixEncoder()
     t.write('ABC')
     t.write('DEF')
-    expect(t.read()).deep.equals(Buffer.from([ 0x00, 0x03, 0x41, 0x42, 0x43, 0x00, 0x03, 0x44, 0x45, 0x46 ]))
+    expect(t.read()).deep.equals(Buffer.from([ 0x03, 0x00, 0x00, 0x00, 0x41, 0x42, 0x43, 0x03, 0x00, 0x00, 0x00, 0x44, 0x45, 0x46 ]))
     expect(t.read()).equals(null)
   })
 })
